cc054010432992a081dd1c426563b0847f1cc2e1
Diego Gutierrez <diego.gutierrez@buildto.io>
2020-03-05 01:50:41 -0600

    Fix AppCompatTextView

--- app/src/main/java/com/diegogutierrez/seventhart/ui/detail/MovieDetailView.kt
+++ app/src/main/java/com/diegogutierrez/seventhart/ui/detail/MovieDetailView.kt
@@ -2,7 +2,7 @@ package com.diegogutierrez.seventhart.ui.detail
 
 import android.content.Context
 import android.util.AttributeSet
-import android.widget.TextView
+import androidx.appcompat.widget.AppCompatTextView;
 import androidx.core.text.bold
 import androidx.core.text.buildSpannedString
 import com.diegogutierrez.domain.Movie
@@ -11,7 +11,7 @@ class MovieDetailView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : TextView(context, attrs, defStyleAttr) {
+) : AppCompatTextView(context, attrs, defStyleAttr) {
 
     fun setMovie(movie: Movie) = with(movie) {
         text = buildSpannedString {
